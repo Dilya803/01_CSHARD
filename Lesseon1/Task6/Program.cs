4b2d6f60820e038789603c63bbbda0bbf4aab066
user <sajfeevadila@gmail.com>
2024-02-16 12:22:03 +0300

    add Homework

---
+++ Lesseon1/Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 10;
+int b = 60;
+int c = 31;
+double d = 400;
+
+int product1 = a * b;
+
+Console.WriteLine (product1);
+
+double sum1 = c + d;
+
+Console.WriteLine (sum1);
+
+double quolent1 = product1 / sum1;
+
+Console.WriteLine (quolent1);
