4ac81d6e8c1a1a762605dd459578a0d050afcd08
user <sajfeevadila@gmail.com>
2024-02-27 22:06:00 +0300

    add Lesson4:task3

---
+++ Lesson4/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLettersFromStr(string s)
+{
+    string Letters = "";
+    foreach (char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            Letters = Letters + e;
+        }
+    }
+    return Letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromStr(str);
+Console.WriteLine(result);
