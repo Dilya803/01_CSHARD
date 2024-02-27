8a9ab9aa913670e4ba54c70b2c8503567e6c850e
user <sajfeevadila@gmail.com>
2024-02-27 21:04:00 +0300

    add Lesson4:task1

---
+++ Lesson4/task1/Program.cs
@@ -0,0 +1,49 @@
+﻿//int[,] maxtrix = new int[3, 5];
+
+//Random rnd = new Random();
+//for (int i = 0; i < maxtrix.GetLength(0); i++)
+//{
+    //for (int j = 0; j < maxtrix.GetLength(1); j++)
+    //{
+       // maxtrix[i, j] = rnd.Next(1, 11);
+    //}
+//}
+
+//for (int i = 0; i < maxtrix.GetLength(0); i++)
+//{
+    //for (int j = 0; j < maxtrix.GetLength(1); j++)
+    //{
+        //Console.Write($"{maxtrix[i, j]} ");
+    //}
+    //Console.WriteLine();
+//}
+
+int[,] CreatMaxtrix(int rowCount, int columsCount)
+{
+    int[,] maxtrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < maxtrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < maxtrix.GetLength(1); j++)
+        {
+            maxtrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return maxtrix;
+}
+
+void ShowMaxtrix(int[,] maxtrix)
+{
+    for (int i = 0; i < maxtrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < maxtrix.GetLength(1); j++)
+        {
+            Console.Write($"{maxtrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] maxtrix = CreatMaxtrix(4, 5);
+ShowMaxtrix(maxtrix);
